8e6df33be561492c96aa027c5498a6446b96603a
gmunglani <gmunglani@gmail.com>
2017-08-30 07:18:55 +0200

    Improved error handling, fixed orientation function

--- find_orient.m
+++ find_orient.m
@@ -1,7 +1,5 @@
 function type = find_orient(M)
 
 maxx = size(M,1); maxy = size(M,2); type = 0;
-if ~isempty(nonzeros(M(1,:))) type = 1; end
-if ~isempty(nonzeros(M(:,maxy))) type = 2; end
-if ~isempty(nonzeros(M(maxx,:))) type = 3; end
-if ~isempty(nonzeros(M(:,1))) type = 4; end
+places = [sum(M(1,:)) sum(M(:,maxy)) sum(M(maxx,:)) sum(M(:,1))];
+[temp type] = max(places);
--- video_processing.m
+++ video_processing.m
@@ -1,11 +1,11 @@
-function video_processing(movie,framerange,BT1,BT2,framerate,timestep,Cmax,Cmin,M)
+function video_processing(movie,stp,smp,BT1,BT2,framerate,timestep,Cmax,Cmin,M)
 
 movie = [movie '.avi'];
 V = VideoWriter(movie);
 V.FrameRate = framerate;
 open(V);    
 
-K = M(:,:,1:framerange)./Cmax;
+K = M(:,:,stp:smp)./Cmax;
 K(isnan(K)) = 0;
 Cmin = Cmin/Cmax;
 
@@ -14,11 +14,11 @@ L = bsxfun(@rdivide, bsxfun(@minus, K, Cmin),bsxfun(@minus, 1, Cmin));
 L(L<0) = 0;
 L = uint8(L.*255);
 
+figure
 map = colormap(jet(255));
 map = vertcat([0 0 0],map);
-
-figure
 for count = 1:size(L,3)
+    disp(['Video Processing:' num2str(count)]);
     imshow(L(:,:,count),map);
     txtstr = strcat('Frame: ',num2str(count*timestep));
     text(10,10,txtstr,'color','white')
