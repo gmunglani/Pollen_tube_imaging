23b7ab16eff01ae4f66458aec4732b74afcb6b31
gmunglani <gmunglani@gmail.com>
2017-12-14 19:01:10 +0100

    Added ellipse finding

--- ellipse_view.m
+++ ellipse_view.m
@@ -1,26 +1,33 @@
-function ellipse_view(s)
+function ellipse_view2(center,phin,axes)
 
-phi = linspace(0,2*pi,50);
+phi = linspace(0,2*pi,180);
 cosphi = cos(phi);
 sinphi = sin(phi);
 
-for k = 1:length(s)
-    xbar = s(k).Centroid(1);
-    ybar = s(k).Centroid(2);
-
-    a = s(k).MajorAxisLength/2;
-    b = s(k).MinorAxisLength/2;
-
-    theta = pi*s(k).Orientation/180;
-    R = [ cos(theta)   sin(theta)
-         -sin(theta)   cos(theta)];
+for k = 1:length(phi)
+    xbar = center(1);
+    ybar = center(2);
+    theta = phin;
+    
+    if (axes(2) > axes(1))
+        a = axes(1);
+        b = axes(2);
+    else
+        a = axes(2);
+        b = axes(1);
+    end
+            
+    R = [ cos(theta)  sin(theta)
+          -sin(theta)   cos(theta)];
 
     xy = [a*cosphi; b*sinphi];
-    xy = R*xy;
+    xy = R'*xy;
 
     x = xy(1,:) + xbar;
     y = xy(2,:) + ybar;
-
-    plot(x,y,'y','LineWidth',2);
+    
+    plot(y,x,'y','LineWidth',2);
 end
 
+%Lines=[(1:size(x,2)); (2:size(x,2)+1)]'; Lines(end,2)=1;
+%curve = LineCurvature2D([x y],Lines);
